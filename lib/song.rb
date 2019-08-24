0991fe5979d71063d2600d83aabce94d01b52dc0
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-24 20:18:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    song.find_by_name(song_name) || self.create_by_name
+    song.find_by_name(song_name) || song.create_by_name
   end
 
 end
