f6aa4f3c556b052c8f6e80d92ce2474fb69c5707
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-24 19:55:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
   
-  def self.new_by_name(name)
-    song = self.new_by_name
-    @@all << song
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
     song.name
   end
 
